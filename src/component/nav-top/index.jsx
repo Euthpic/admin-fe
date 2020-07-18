f321ca5682e12652a62c509848263b5b6c2b2091
Euthpic <490114771@qq.com>
2020-07-18 21:19:06 +0800

    侧边导航

--- src/component/nav-top/index.jsx
+++ src/component/nav-top/index.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 class NavTop extends React.Component {
     constructor(props) {
         super(props);
     }
 
-     onLogout() {
-        
-    }
-
+    onLogout() {}
 
     render() {
         return (
-            <div className="navbar navbar-default top-navbar" >
+            <div className="navbar navbar-default top-navbar">
                 <div className="navbar-header">
 
-                    <Link className="navbar-brand" to="/"><b>I</b>vy</Link>
+                    <Link className="navbar-brand" to="/">
+                        <b>I</b>vy</Link>
                 </div>
 
                 <ul className="nav navbar-top-links navbar-right">
                     <li className="dropdown">
-                        <a className="dropdown-toggle" href="javascript:;" >
+                        <a className="dropdown-toggle" href="javascript:;">
                             <i className="fa fa-user fa-fw"></i>
                             <span>欢迎,admin</span>
                             <i className="fa fa-caret-down"></i>
                         </a>
                         <ul className="dropdown-menu dropdown-user">
                             <li>
-                                <a onClick={()=>this.onLogout()}>
+                                <a onClick={() => this.onLogout()}>
                                     <i className="fa fa-sign-out fa-fw"></i>
                                     <span>退出登录</span>
                                 </a>
